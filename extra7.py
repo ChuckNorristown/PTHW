404d767688cc03821a8d462b11cf7f9390c811b5
Vince <Bilotta@Newbie-MacBook-Pro-1.local>
2013-04-19 15:20:31 -0400

    adding extra7 to repo

---
+++ extra7.py
@@ -0,0 +1,35 @@
+# displays Mary had a little lamb.
+print "Mary had a little lamb."
+# displays It's fleece was white as snow.
+print "Its fleece was white as %s." % 'snow' 
+# displays And everywhere that Mary went.
+print "And everywhere that Mary went."
+# displays 10 periods
+print "." * 10 # what'd that do?
+# end1 represents C
+end1 = "C"
+#end2 represents h
+end2 = "h"
+#end3 represents e
+end3 = "e"
+#end4 represents e
+end4 = "e"
+#end5 represents s
+end5 = "s"
+#end6 represents e
+end6 = "e"
+#end7 represents B
+end7 = "B"
+# end8 represents u
+end8 = "u"
+#end9 represents r
+end9 = "r"
+#end10 represents g
+end10 = "g"
+#end11 represents e
+end11 = "e"
+#end12 represents r
+end12 = "r"
+
+#will display Cheese Burger
+print end1 + end2 + end3 + end4 + end5 + end6, print end7 + end8 + end9 + end10 + end11 + end12
