4546816db473b617a5ba3dbc2379673bb4465600
Sean Shields <Bilotta@Newbie-MacBook-Pro-1.local>
2013-04-29 17:57:56 -0400

    adding ex45 to repo

---
+++ ex45.py
@@ -0,0 +1,76 @@
+## Animal is-a object
+class Animal(object):
+	pass
+
+## Dog is-a Animal
+class Dog(Animal):
+
+	def __init__(self, name):
+		## Gog has-a name
+		self.name = name
+
+## Cat is-a Animal
+class Cat(Animal):
+
+	def __init__(self, name):
+		## Cat has-a name
+		self.name = name
+
+##
+class Person(object):
+
+	def __init__(self, name):
+		# Person has-a name
+		self.name = name
+
+		## Person has-a pet of some kind
+		self.pet = name
+
+## Employee is a Person
+class Employee(Person):
+
+	def __init__(self, name, salary):
+		## Employee has-a name
+		super(Employee, self).__init__(name)
+		## Employee has-a salary
+		self.salary = salary
+
+## Fish is-a object
+class Fish(object):
+	pass
+
+## Salmon is-a object and a fish
+class Salmon(Fish):
+	pass
+
+	## Halibut is-a object and a fish
+	class Halibut(Fish):
+		pass
+
+
+## rover is a Dog
+rover = Dog("Rover")
+
+## satan is-a cat
+satan = Cat("Satan")
+
+## mary is-a Person
+mary = Person("Mary")
+
+## mary has-a pet named satan 
+mary.pet = satan
+
+## frank is-a employee with 120000 salary
+frank = Employee("Frank", 120000)
+
+## frank has-a pet named rover
+frank.pet = rover
+
+## flipper is-a fish
+flipper = Fish()
+
+## crouse is-s Salmon
+crouse = Salmon()
+
+## harry is-a Halibut
+harry = Halibut
