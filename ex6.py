7beb487484acfa1895c63f4f1a233effeaa9f4ee
Sean Shields <sean@dmgctrl.com>
2013-04-30 12:36:45 -0400

    adding ex6 to repo

--- ex6.py
+++ ex6.py
@@ -1,3 +1,4 @@
+# 
 x = "There are %d types of people." % 10
 binary = "binary"
 do_not = "don't"
@@ -7,7 +8,7 @@ print x
 print y
 
 print "I said: %r." % x
-print "I also said: '%s'" % y
+print "I also said: '%s'." % y
 
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! %r"
@@ -16,5 +17,5 @@ print joke_evaluation % hilarious
 
 w = "This is the left side of..."
 e = "a string with a right side."
-
+# 
 print w + e 
