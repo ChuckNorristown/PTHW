81c4c74f2f31bf0df977fd795de42f2a10c5dc25
Sean Shields <sean@dmgctrl.com>
2013-04-30 13:06:15 -0400

    finished extra5

---
+++ extra5.py
@@ -0,0 +1,20 @@
+name = 'Sean'
+age = 33 # not a lie
+height = 69.0 # inches
+weight = 170 # lbs
+eyes = 'Hazel'
+teeth = 'White' 
+hair = 'Brown'
+height_feet = 69.0 / 12
+height_cm = 69.0 * 2.54
+weight_kilo = 170 * .453592
+
+print "My name is %r ." % name
+print "I'm am %d inches tall." % height
+print "Or %d feet tall." % height_feet
+print "I weigh %d pounds." % weight
+print "Actually, that's is very appropriate to my height."
+print "I have %r eyes and %r hair." % (eyes, hair)
+print "My teeth are usually %r depending on the coffee." % teeth
+print "In Europe, I am %d centimeters tall." % height_cm
+print "Again in Europe, I weigh %d kilos." % weight_kilo 
