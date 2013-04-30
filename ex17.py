a95ac5cc1ee17f0c2a6774a60f0dc6abf14c2239
Sean Shields <sean@dmgctrl.com>
2013-04-30 14:27:55 -0400

    finished ex17

--- ex17.py
+++ ex17.py
@@ -1,20 +1,20 @@
 from sys import argv
 from os.path import exists
 
-script, Jay.txt, ex15_sample.txt = argv
+script, from_file, to_file = argv
 
-print "Copying from %s to %s" % (jay.txt, ex15_sample.txt)
+print "Copying from %s to %s" % (from_file, to_file)
 
-input = open(Jay.txt)
+input = open(from_file)
 indata = input.read()
 
 print "The input file is %d bytes long" % len(indata)
 
-print "Does the output file exist? %r" % exist(ex15_sample.txt)
+print "Does the output file exist? %r" % exists(to_file)
 print "Ready, hit RETURN to continue, CTRL-C to abort."
 raw_input()
 
-output = open(ex15_sample.txt, 'w')
+output = open(to_file, 'w')
 output.write(indata)
 
 print "Alright, all done."
