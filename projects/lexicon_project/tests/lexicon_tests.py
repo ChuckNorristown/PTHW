17e2821073b31755497cb2511a66b9b9cbec5805
Sean Shields <sean@dmgctrl.com>
2013-05-02 11:30:22 -0400

    extra credit version

--- projects/lexicon_project/tests/lexicon_tests.py
+++ projects/lexicon_project/tests/lexicon_tests.py
@@ -4,32 +4,36 @@ from ex48 import lexicon
 
 def test_directions():
     assert_equal(lexicon.scan("north"), [('direction', 'north')])
-    result = lexicon.scan("north south east")
+    result = lexicon.scan("north south east west")
     assert_equal(result, [('direction', 'north'),
                           ('direction', 'south'),
-                          ('direction', 'east')])
+                          ('direction', 'east'),
+                          ('direction', 'west')])
 
 def tests_verbs():
     assert_equal(lexicon.scan("go"), [('verb', 'go')])
-    result = lexicon.scan("go kill eat")
+    result = lexicon.scan("go kill eat sleep")
     assert_equal(result, [('verb', 'go'),
                           ('verb', 'kill'),
-                          ('verb', 'eat')])
+                          ('verb', 'eat'),
+                          ('verb', 'sleep')])
 
 
 def tests_stops():
     assert_equal(lexicon.scan("the"), [('stop', 'the')])
-    result = lexicon.scan("the in of")
+    result = lexicon.scan("the in of out")
     assert_equal(result, [('stop', 'the'),
                            ('stop', 'in'),
-                           ('stop', 'of')])
+                           ('stop', 'of'),
+                           ('stop', 'out')])
 
 def test_nouns():
     assert_equal(lexicon.scan("bear"), [('noun', 'bear')])
-    result = lexicon.scan("bear princess")
+    result = lexicon.scan("bear princess cave castle")
     assert_equal(result, [('noun', 'bear'),
-                          ('noun', 'princess')])
-
+                          ('noun', 'princess'),
+                          ('noun', 'cave'),
+                          ('noun', 'castle')])
 
 def test_numbers():
     assert_equal(lexicon.scan("1234"), [('number', 1234)])
