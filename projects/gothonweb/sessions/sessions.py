e5ae779f67cdae8e0a0ac0a2c71167f579579557
Sean Shields <sean@dmgctrl.com>
2013-05-24 17:26:41 -0400

    adding sessions to repo

---
+++ projects/gothonweb/sessions/sessions.py
@@ -0,0 +1,24 @@
+import web
+
+web.config.debug = False
+
+urls = (
+	"/count", "count",
+	"/reset", "reset"
+)
+app = web.application(urls, locals())
+store = web.sessionsDiskStore('sessions')
+sessions = web.sessions.Sessions(app, store, initializer={'count': 0})
+
+class count:
+	def GET(self):
+		session.count += 1
+		return str(session.count)
+
+class reset:
+	def GET(self):
+		session.kill()
+		return ""
+
+if __name__ == "__main__":
+	app.run()
